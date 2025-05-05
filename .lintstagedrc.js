9c520740f48856a6733c19aa36e08d998d93ab21
dk <dakorsun@gmail.com>
2025-05-05 17:11:46 +0300

    build(ci): extended lint-staged scope

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -6,6 +6,10 @@ const buildEslintCommand = (filenames) =>
 		.join(' --file ')}`
 
 const config = {
+	// Run ESLint on JS/TS files
 	'*.{js,jsx,ts,tsx}': [buildEslintCommand],
+
+	// Run Prettier on other file types
+	'*.{json,md,html,css,scss,yml,yaml}': ['prettier --write'],
 }
 export default config;
